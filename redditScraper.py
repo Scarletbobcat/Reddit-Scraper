538aa1c1b6e7f97a41ee6e7ff65abde7a314184b
Tien Hoang <105090250+Scarletbobcat@users.noreply.github.com>
2022-11-16 19:34:48 -0500

    New file for example, update scraper
    scraper now adds a comments header to the csv file and adds all comments from the post 
    also sorts the subreddit by time and past year

--- redditScraper.py
+++ redditScraper.py
@@ -3,27 +3,34 @@ import praw
 reddit = praw.Reddit(client_id='hdcvNosfFtsG7qWKTvz9yg', client_secret='qCvJOwr709QWgqHA-pV_Zvb4EviPOg', user_agent='Testing')
 
 sub1 = reddit.subreddit('EngineeringStudents')
-
 sub2 = reddit.subreddit('drones')
 
 filename = "drones&engineering.csv"
 
 f = open(filename, "w")
 
-headers = "Post Title, Post Url\n"
+headers = "Post Title, Post Url, Post Desc, Post Comments ->\n"
 
 f.write(headers)
 
-for submission in sub1.search("drones", sort="hot", limit=10):
-    title = submission.title
+for submission in sub1.search("drone", sort="top", time_filter="year", limit=5):
+    title = submission.title.replace(',', '')
     url = submission.url
-    title = title.replace(',', '')
-    f.write(title + ',' + url + '\n')
+    f.write(title + ',' + url + ',' + submission.selftext.replace('\n', ' ').replace(',', '') + ',')
+    for top_comment in submission.comments:
+        body = top_comment.body.replace(',', '').replace('\n', ' ')
+        f.write(body + ',')
+    f.write('\n')
 
-for submission in sub2.search("engineer", sort="hot", limit=10):
-    title = submission.title
+for submission in sub2.search("engineer", sort="top", time_filter="year", limit=5):
+    title = submission.title.replace(',', '').replace('\n', ' ')
     url = submission.url
-    title = title.replace(',', '')
-    f.write(title + ',' + url + '\n')
+    f.write(title + ',' + url + ',' + submission.selftext.replace('\n', ' ').replace(',', '') + ',')
+    for top_comment in submission.comments:
+        body = top_comment.body.replace(',', '').replace('\n', ' ')
+        f.write(body + ',')
+    f.write('\n')
 
 f.close()
+
+# add top comments
